152e860540a4f660ba94dfdadc63a8f0cea6b9a7
Alessandro Capelli <me@alessandrocapelli.it>
2021-03-26 01:04:19 +0100

    Update admin

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -4,14 +4,10 @@ from .models import CarMake, CarModel
 
 class CarModelInline(admin.StackedInline):
     model = CarModel
-    extra = 5
+    extra = 1
 
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
-    list_display = ['name']
 
-class CarModelAdmin(admin.ModelAdmin):
-    list_display = ['carmake']
-
+admin.site.register(CarModel)
 admin.site.register(CarMake, CarMakeAdmin)
-admin.site.register(CarModel, CarModelAdmin)
